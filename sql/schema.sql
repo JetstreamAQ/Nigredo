96593170410349e2f8612fcb5fbcd2f20cd79c0a
JetstreamAQ <45304769+JetstreamAQ@users.noreply.github.com>
2022-06-13 23:00:18 +0000

    some more formatting

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,19 +2,19 @@ CREATE SCHEMA `mediaDB`;
 
 CREATE TABLE Media (
 	url VARCHAR(256) NOT NULL,
-    PRIMARY KEY (url)
+	PRIMARY KEY (url)
 );
 
 CREATE TABLE Tag (
 	name VARCHAR(64) NOT NULL,
-    Primary Key (name)
+	Primary Key (name)
 );
 
 CREATE TABLE MediaTag (
 	url VARCHAR(256) NOT NULL,
-    name VARCHAR(64) NOT NULL,
-    PRIMARY KEY (url, name),
-    FOREIGN KEY (url)
+	name VARCHAR(64) NOT NULL,
+	PRIMARY KEY (url, name),
+	FOREIGN KEY (url)
 		REFERENCES Media (url),
 	FOREIGN KEY (name)
 		REFERENCES Tag (name)
